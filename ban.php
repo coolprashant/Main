fbffe77ffec7e72941f0a1fa7d34fde82a44b252
coolprashant <cloud.strifextiffa@gmail.com>
2014-03-15 08:24:15 +0530

    Create ban.php

---
+++ ban.php
@@ -0,0 +1,15 @@
+            <li class="_54ni __MenuItem" aria-selected="false">
+                <a class="_54nc" tabindex="0" href="/ajax/groups/members/remove.php?group_id=598368236842941&uid=100006230942059" rel="dialog-post" role="menuitem">
+                    <span>
+                        <span class="_54nh">
+
+                            Remove from Group
+
+                        </span>
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+</div>
